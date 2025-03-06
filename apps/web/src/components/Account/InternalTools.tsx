20fa163e15689b0a724409ca632a30d9180c042f
Bigint <69431456+bigint@users.noreply.github.com>
2025-03-06 16:07:27 +0530

    refactor: Modularize InternalTools with Tools rendering helper

--- apps/web/src/components/Account/InternalTools.tsx
+++ apps/web/src/components/Account/InternalTools.tsx
@@ -18,14 +18,20 @@ const InternalTools: FC<InternalToolsProps> = ({ account }) => {
     return null;
   }
 
+  const Tools = () => (
+    <>
+      {hasCreatorToolAccess && <CreatorTool account={account} />}
+      {isStaff && <StaffTool account={account} />}
+    </>
+  );
+
   return (
     <Card
       as="aside"
       className="!bg-yellow-300/20 mb-4 space-y-5 border-yellow-400 p-5 text-yellow-600"
       forceRounded
     >
-      {hasCreatorToolAccess && <CreatorTool account={account} />}
-      {isStaff && <StaffTool account={account} />}
+      <Tools />
     </Card>
   );
 };
