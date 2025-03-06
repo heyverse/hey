db8d9c44b23135e4d4c4fecbe6f2144de80b53cc
Bigint <69431456+bigint@users.noreply.github.com>
2025-03-06 15:10:17 +0530

    refactor: Improve error handling in catchedError helper with destructured error object

--- apps/api/src/helpers/catchedError.ts
+++ apps/api/src/helpers/catchedError.ts
@@ -1,13 +1,16 @@
 import logger from "@hey/helpers/logger";
 import type { Response } from "express";
 
-const catchedError = (res: Response, error: any, status?: number) => {
-  const statusCode = status || 500;
-  logger.error(error);
+const catchedError = (
+  res: Response,
+  { message, ...rest }: any,
+  status = 500
+) => {
+  logger.error(rest);
 
-  return res.status(statusCode).json({
-    error: statusCode < 500 ? "client_error" : "server_error",
-    message: error.message,
+  return res.status(status).json({
+    error: status < 500 ? "client_error" : "server_error",
+    message,
     success: false
   });
 };
