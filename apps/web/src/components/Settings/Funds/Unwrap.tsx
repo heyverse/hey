62f1ff7a3fc8f9bfdfbc841f615be6b77f4d39ac
Bigint <69431456+bigint@users.noreply.github.com>
2025-03-17 20:04:47 +0530

    feat: Integrate modal for token unwrapping with input handling in Unwrap component

--- apps/web/src/components/Settings/Funds/Unwrap.tsx
+++ apps/web/src/components/Settings/Funds/Unwrap.tsx
@@ -2,7 +2,7 @@ import trackEvent from "@helpers/analytics";
 import errorToast from "@helpers/errorToast";
 import { Events } from "@hey/data/events";
 import { useUnwrapTokensMutation } from "@hey/indexer";
-import { Button } from "@hey/ui";
+import { Button, Input, Modal } from "@hey/ui";
 import { type FC, useState } from "react";
 import toast from "react-hot-toast";
 import usePollTransactionStatus from "src/hooks/usePollTransactionStatus";
@@ -15,12 +15,15 @@ interface UnwrapProps {
 
 const Unwrap: FC<UnwrapProps> = ({ value, refetch }) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [valueToUnwrap, setValueToUnwrap] = useState(value);
   const handleTransactionLifecycle = useTransactionLifecycle();
   const pollTransactionStatus = usePollTransactionStatus();
 
   const onCompleted = (hash: string) => {
     setIsSubmitting(false);
     trackEvent(Events.Account.UnwrapTokens);
+    setShowModal(false);
     toast.success("Unwrap Initiated");
     pollTransactionStatus(hash, () => {
       refetch();
@@ -48,23 +51,51 @@ const Unwrap: FC<UnwrapProps> = ({ value, refetch }) => {
     onError
   });
 
-  const handleUnwrap = (value: string) => {
+  const handleUnwrap = () => {
     setIsSubmitting(true);
 
     return unwrapTokens({
-      variables: { request: { amount: value } }
+      variables: { request: { amount: valueToUnwrap } }
     });
   };
 
   return (
-    <Button
-      size="sm"
-      outline
-      onClick={() => handleUnwrap(value)}
-      disabled={isSubmitting}
-    >
-      Unwrap
-    </Button>
+    <>
+      <Button
+        size="sm"
+        outline
+        onClick={() => setShowModal(true)}
+        disabled={isSubmitting}
+      >
+        Unwrap
+      </Button>
+      <Modal
+        title="Unwrap"
+        show={showModal}
+        onClose={() => setShowModal(false)}
+      >
+        <div className="p-5">
+          <div className="mb-5 flex items-center gap-2">
+            <Input
+              type="number"
+              value={valueToUnwrap}
+              onChange={(e) => setValueToUnwrap(e.target.value)}
+            />
+            <Button size="lg" onClick={() => setValueToUnwrap(value)}>
+              Max
+            </Button>
+          </div>
+          <Button
+            className="w-full"
+            size="lg"
+            onClick={handleUnwrap}
+            disabled={isSubmitting || !valueToUnwrap || valueToUnwrap === "0"}
+          >
+            Unwrap
+          </Button>
+        </div>
+      </Modal>
+    </>
   );
 };
 
