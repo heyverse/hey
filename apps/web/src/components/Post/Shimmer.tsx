01471282649c2bba9cd7c10e774c9137028fe7dc
Yoginth <yoginth@hey.com>
2025-04-05 20:43:15 +0530

    Refactor PublicationPageShimmer component to integrate GeneralPageLayout

--- apps/web/src/components/Post/Shimmer.tsx
+++ apps/web/src/components/Post/Shimmer.tsx
@@ -3,12 +3,8 @@ import PostListShimmer from "@/components/Shared/Shimmer/PostListShimmer";
 import PostShimmer from "@/components/Shared/Shimmer/PostShimmer";
 import PostsShimmer from "@/components/Shared/Shimmer/PostsShimmer";
 import SingleAccountShimmer from "@/components/Shared/Shimmer/SingleAccountShimmer";
-import {
-  Card,
-  GridItemEight,
-  GridItemFour,
-  GridLayout
-} from "@/components/Shared/UI";
+import { Card } from "@/components/Shared/UI";
+import { GeneralPageLayout } from "../Shared/PageLayout";
 
 interface PublicationPageShimmerProps {
   publicationList?: boolean;
@@ -18,37 +14,38 @@ const PublicationPageShimmer = ({
   publicationList = false
 }: PublicationPageShimmerProps) => {
   return (
-    <GridLayout>
-      <GridItemEight className="space-y-5">
-        {publicationList ? (
-          <PostListShimmer />
-        ) : (
-          <>
-            <Card>
-              <PostShimmer />
-            </Card>
-            <PostsShimmer />
-          </>
-        )}
-      </GridItemEight>
-      <GridItemFour className="space-y-5">
-        <Card className="p-5">
-          <SingleAccountShimmer />
-        </Card>
-        <Card className="space-y-4 p-5">
-          <SingleAccountShimmer showFollowUnfollowButton />
-          <SingleAccountShimmer showFollowUnfollowButton />
-          <SingleAccountShimmer showFollowUnfollowButton />
-          <SingleAccountShimmer showFollowUnfollowButton />
-          <SingleAccountShimmer showFollowUnfollowButton />
-        </Card>
-        <Card className="flex justify-between p-5">
-          <div className="shimmer h-3 w-1/2 rounded-lg" />
-          <div className="shimmer h-3 w-1/4 rounded-lg" />
-        </Card>
-        <Footer />
-      </GridItemFour>
-    </GridLayout>
+    <GeneralPageLayout
+      sidebar={
+        <div className="space-y-5">
+          <Card className="p-5">
+            <SingleAccountShimmer />
+          </Card>
+          <Card className="space-y-4 p-5">
+            <SingleAccountShimmer showFollowUnfollowButton />
+            <SingleAccountShimmer showFollowUnfollowButton />
+            <SingleAccountShimmer showFollowUnfollowButton />
+            <SingleAccountShimmer showFollowUnfollowButton />
+            <SingleAccountShimmer showFollowUnfollowButton />
+          </Card>
+          <Card className="flex justify-between p-5">
+            <div className="shimmer h-3 w-1/2 rounded-lg" />
+            <div className="shimmer h-3 w-1/4 rounded-lg" />
+          </Card>
+          <Footer />
+        </div>
+      }
+    >
+      {publicationList ? (
+        <PostListShimmer />
+      ) : (
+        <>
+          <Card>
+            <PostShimmer />
+          </Card>
+          <PostsShimmer />
+        </>
+      )}
+    </GeneralPageLayout>
   );
 };
 
