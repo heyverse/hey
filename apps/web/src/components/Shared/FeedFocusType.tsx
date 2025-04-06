f3ba95b75ce26f94615b62ed740506cfd729d803
Yoginth <yoginth@hey.com>
2025-04-06 22:11:25 +0530

    Refactor FeedFocusType and TabButton components to improve layout and consistency

--- apps/web/src/components/Shared/FeedFocusType.tsx
+++ apps/web/src/components/Shared/FeedFocusType.tsx
@@ -8,31 +8,36 @@ interface FeedFocusTypeProps {
 }
 
 const FeedFocusType = ({ focus, setFocus }: FeedFocusTypeProps) => (
-  <div className="mx-5 flex flex-wrap gap-3 sm:mx-0">
+  <div className="mx-5 flex flex-wrap gap-3 md:mx-0">
     <TabButton
       active={!focus}
       name="All posts"
       onClick={() => setFocus(undefined)}
+      showOnSm
     />
     <TabButton
       active={focus === MainContentFocus.TextOnly}
       name="Text"
       onClick={() => setFocus(MainContentFocus.TextOnly)}
+      showOnSm
     />
     <TabButton
       active={focus === MainContentFocus.Video}
       name="Video"
       onClick={() => setFocus(MainContentFocus.Video)}
+      showOnSm
     />
     <TabButton
       active={focus === MainContentFocus.Audio}
       name="Audio"
       onClick={() => setFocus(MainContentFocus.Audio)}
+      showOnSm
     />
     <TabButton
       active={focus === MainContentFocus.Image}
       name="Images"
       onClick={() => setFocus(MainContentFocus.Image)}
+      showOnSm
     />
   </div>
 );
