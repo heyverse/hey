637a7cab0ebdafa9db97112b1eefd613517f7683
Yoginth <yoginth@hey.com>
2025-04-11 17:58:37 +0530

    chore: Replace 'react-hot-toast' with 'sonner' for toast

--- apps/web/src/components/Common/Layout.tsx
+++ apps/web/src/components/Common/Layout.tsx
@@ -4,7 +4,6 @@ import GlobalModals from "@/components/Shared/GlobalModals";
 import Navbar from "@/components/Shared/Navbar";
 import BottomNavigation from "@/components/Shared/Navbar/BottomNavigation";
 import getCurrentSession from "@/helpers/getCurrentSession";
-import getToastOptions from "@/helpers/getToastOptions";
 import { useTheme } from "@/hooks/useTheme";
 import { useAccountStatus } from "@/store/non-persisted/useAccountStatus";
 import { useAccountStore } from "@/store/persisted/useAccountStore";
@@ -13,8 +12,8 @@ import { usePreferencesStore } from "@/store/persisted/usePreferencesStore";
 import { useMeQuery } from "@hey/indexer";
 import { useIsClient } from "@uidotdev/usehooks";
 import { useEffect } from "react";
-import { Toaster } from "react-hot-toast";
 import { Outlet, useLocation } from "react-router";
+import { Toaster, type ToasterProps } from "sonner";
 
 const Layout = () => {
   const { pathname } = useLocation();
@@ -59,11 +58,7 @@ const Layout = () => {
 
   return (
     <>
-      <Toaster
-        containerStyle={{ wordBreak: "break-word" }}
-        position="bottom-right"
-        toastOptions={getToastOptions(theme)}
-      />
+      <Toaster position="bottom-right" theme={theme as ToasterProps["theme"]} />
       <GlobalModals />
       <GlobalAlerts />
       <div className="mx-auto flex w-full max-w-6xl items-start gap-x-8 px-0 md:px-5">
