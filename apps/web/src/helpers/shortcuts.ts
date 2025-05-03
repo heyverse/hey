3525cec489770fd6050d01dfd638dbd11fb0230b
Yoginth <hey@yoginth.com>
2025-05-03 12:38:04 +0530

    Add keyboard shortcuts for creating posts in NewPublication component

---
+++ apps/web/src/helpers/shortcuts.ts
@@ -0,0 +1,9 @@
+const KeyboardShortcuts = {
+  ThisModal: { key: "?", name: "This Modal" },
+  CreatePost: {
+    key: "mod+enter",
+    name: "Create Post/Comment"
+  }
+};
+
+export default KeyboardShortcuts;
