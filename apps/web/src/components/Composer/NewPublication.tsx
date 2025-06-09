96afc1a639cc2cd998883219d9f8252b500084c1
Yoginth <hey@yoginth.com>
2025-06-09 12:12:41 +0530

    Update visibility logic for RulesSettings in NewPublication component to conditionally render based on editingPost state

--- apps/web/src/components/Composer/NewPublication.tsx
+++ apps/web/src/components/Composer/NewPublication.tsx
@@ -260,7 +260,7 @@ const NewPublication = ({ className, post, feed }: NewPublicationProps) => {
           <QuotedPost isNew post={quotedPost} />
         </Wrapper>
       ) : null}
-      <RulesSettings />
+      {editingPost ? null : <RulesSettings />}
       <div className="divider mx-5" />
       <div className="block items-center px-5 py-3 sm:flex">
         <div className="flex items-center space-x-4">
