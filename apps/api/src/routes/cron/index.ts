d8e0c88e029bdee969df258c3d2c697f721dfc42
Yoginth <hey@yoginth.com>
2025-07-16 10:25:23 +0530

    refactor: remove syncFollowersStandingToGuild route and related file to streamline cron functionality

--- apps/api/src/routes/cron/index.ts
+++ apps/api/src/routes/cron/index.ts
@@ -1,17 +1,11 @@
 import { Hono } from "hono";
 import secretMiddleware from "@/middlewares/secretMiddleware";
-import syncFollowersStandingToGuild from "./guild/syncFollowersStandingToGuild";
 import syncSubscribersToGuild from "./guild/syncSubscribersToGuild";
 import removeExpiredSubscribers from "./removeExpiredSubscribers";
 
 const app = new Hono();
 
 app.get("/syncSubscribersToGuild", secretMiddleware, syncSubscribersToGuild);
-app.get(
-  "/syncFollowersStandingToGuild",
-  secretMiddleware,
-  syncFollowersStandingToGuild
-);
 app.get(
   "/removeExpiredSubscribers",
   secretMiddleware,
