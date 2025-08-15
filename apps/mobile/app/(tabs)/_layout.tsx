c232010eb6a9f0333218bae940b447450607a28d
Yoginth <hey@yoginth.com>
2025-08-15 16:12:16 +0530

    feat: update new mobile app

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -24,7 +24,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           tabBarIcon: () => <HomeIcon className="size-6" />,
           tabBarLabel: () => null
