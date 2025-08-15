d448fe723602587e25c8a3399c2a910629b457a8
Yoginth <hey@yoginth.com>
2025-08-15 15:04:50 +0530

    feat: update new mobile app

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,14 +1,11 @@
 import { Tabs } from "expo-router";
 import { Platform } from "react-native";
 
-import { HapticTab } from "@/components/HapticTab";
-
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarButton: HapticTab,
         tabBarStyle: Platform.select({
           default: {},
           ios: { position: "absolute" }
