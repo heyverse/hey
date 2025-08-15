27e60941c09104324988bb6972e945918310e305
Yoginth <hey@yoginth.com>
2025-08-15 15:03:43 +0530

    feat: update new mobile app

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -2,48 +2,21 @@ import { Tabs } from "expo-router";
 import { Platform } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        tabBarBackground: TabBarBackground,
         tabBarButton: HapticTab,
         tabBarStyle: Platform.select({
           default: {},
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: "absolute"
-          }
+          ios: { position: "absolute" }
         })
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <IconSymbol color={color} name="house.fill" size={28} />
-          ),
-          title: "Home"
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <IconSymbol color={color} name="paperplane.fill" size={28} />
-          ),
-          title: "Explore"
-        }}
-      />
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="explore" options={{ title: "Explore" }} />
     </Tabs>
   );
 }
