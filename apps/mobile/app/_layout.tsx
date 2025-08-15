7b470dec0bb687301a77846b60b604ce9575b2d1
Yoginth <hey@yoginth.com>
2025-08-15 16:02:27 +0530

    feat: update new mobile app

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,29 +1,15 @@
+import "../global.css";
 import { ApolloProvider } from "@apollo/client";
 import { createApolloClient } from "@hey/indexer/apollo/client";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import authLink from "@/helpers/authLink";
-import "../global.css";
+import App from "./App";
 
 const lensApolloClient = createApolloClient(authLink);
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <ApolloProvider client={lensApolloClient}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <App />
     </ApolloProvider>
   );
 }
