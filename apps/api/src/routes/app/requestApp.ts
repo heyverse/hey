00f76dcdf6952afef92dbe1043f34f86d36278bc
Yoginth <hey@yoginth.com>
2025-08-25 06:20:22 -0700

    feat: add ApiError for API routes

--- apps/api/src/routes/app/requestApp.ts
+++ apps/api/src/routes/app/requestApp.ts
@@ -2,6 +2,7 @@ import { Status } from "@hey/data/enums";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import type { Context } from "hono";
 import prisma from "@/prisma/client";
+import ApiError from "@/utils/apiError";
 import handleApiError from "@/utils/handleApiError";
 import { delRedis } from "@/utils/redis";
 
@@ -10,9 +11,18 @@ const requestApp = async (ctx: Context) => {
     const { email } = await ctx.req.json();
     const account = ctx.get("account");
 
-    const appRequest = await prisma.appRequest.create({
-      data: { accountAddress: account as string, email }
-    });
+    const appRequest = await prisma.appRequest
+      .create({ data: { accountAddress: account as string, email } })
+      .catch((error: unknown) => {
+        if (
+          error instanceof PrismaClientKnownRequestError &&
+          error.code === "P2002"
+        ) {
+          throw new ApiError(409, "Email already requested");
+        }
+
+        throw error;
+      });
 
     await delRedis(`app-request:${account}`);
 
@@ -25,15 +35,6 @@ const requestApp = async (ctx: Context) => {
       status: Status.Success
     });
   } catch (error) {
-    if (error instanceof PrismaClientKnownRequestError) {
-      if (error.code === "P2002") {
-        return ctx.json({
-          error: "Email already requested",
-          status: Status.Error
-        });
-      }
-    }
-
     return handleApiError(ctx, error);
   }
 };
