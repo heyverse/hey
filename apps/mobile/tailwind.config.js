bd9ebf89bb6b7b8e19a5bdc17225f0ecfd4a0099
Yoginth <hey@yoginth.com>
2025-09-01 00:38:07 +0530

    Remove Mobile

--- apps/mobile/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-// apps/mobile/tailwind.config.js
-module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  plugins: [],
-  presets: [require("nativewind/preset")],
-  theme: { extend: {} }
-};
