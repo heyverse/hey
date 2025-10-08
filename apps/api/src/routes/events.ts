775620e20a3df76653692ac2a94c445b2186ce9b
Yoginth <hey@yoginth.com>
2025-10-09 00:40:19 +0530

    feat: include owner in logEvent integration for event actions

--- apps/api/src/routes/events.ts
+++ apps/api/src/routes/events.ts
@@ -10,6 +10,7 @@ const events = async (ctx: Context) => {
     const ipData = getIpData(ctx);
     const country = ipData.countryCode;
     const region = ipData.region;
+    const owner = ctx.get("owner");
 
     if (!event) {
       ctx.status(400);
@@ -17,7 +18,7 @@ const events = async (ctx: Context) => {
     }
 
     await fetch("https://yoginth.com/api/hey/events", {
-      body: JSON.stringify({ country, event, region }),
+      body: JSON.stringify({ country, event, owner, region }),
       headers: {
         accept: "application/json",
         "content-type": "application/json"
