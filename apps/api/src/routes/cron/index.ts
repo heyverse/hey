872e8f84faadc89cc7591e4a01fbb7cc27868fbf
Yoginth <hey@yoginth.com>
2025-10-24 18:14:58 +0530

    feat: add syncHeyNamesMembersToGuild route to cron API for syncing HeyNames members to guild

--- apps/api/src/routes/cron/index.ts
+++ apps/api/src/routes/cron/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import secretMiddleware from "@/middlewares/secretMiddleware";
 import syncBetaMembersToGuild from "./guild/syncBetaMembersToGuild";
+import syncHeyNamesMembersToGuild from "./guild/syncHeyNamesMembersToGuild";
 import syncSubscribersToGuild from "./guild/syncSubscribersToGuild";
 import totalSubscribers from "./guild/totalSubscribers";
 import removeExpiredSubscribers from "./removeExpiredSubscribers";
@@ -9,6 +10,11 @@ const app = new Hono();
 
 app.get("/syncBetaMembersToGuild", secretMiddleware, syncBetaMembersToGuild);
 app.get("/syncSubscribersToGuild", secretMiddleware, syncSubscribersToGuild);
+app.get(
+  "/syncHeyNamesMembersToGuild",
+  secretMiddleware,
+  syncHeyNamesMembersToGuild
+);
 app.get("/totalSubscribers", totalSubscribers);
 app.get(
   "/removeExpiredSubscribers",
