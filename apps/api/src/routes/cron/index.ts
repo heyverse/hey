cc0d6026481cfbe154a6473f08baa1db1c6a72f1
Yoginth <hey@yoginth.com>
2025-11-14 22:00:56 +0530

    refactor: remove unused guild synchronization routes and related utility functions from API

--- apps/api/src/routes/cron/index.ts
+++ apps/api/src/routes/cron/index.ts
@@ -1,21 +1,9 @@
 import { Hono } from "hono";
 import secretMiddleware from "@/middlewares/secretMiddleware";
-import syncBetaMembersToGuild from "./guild/syncBetaMembersToGuild";
-import syncHeyNamesMembersToGuild from "./guild/syncHeyNamesMembersToGuild";
-import syncSubscribersToGuild from "./guild/syncSubscribersToGuild";
-import totalSubscribers from "./guild/totalSubscribers";
 import removeExpiredSubscribers from "./removeExpiredSubscribers";
 
 const app = new Hono();
 
-app.get("/syncBetaMembersToGuild", secretMiddleware, syncBetaMembersToGuild);
-app.get("/syncSubscribersToGuild", secretMiddleware, syncSubscribersToGuild);
-app.get(
-  "/syncHeyNamesMembersToGuild",
-  secretMiddleware,
-  syncHeyNamesMembersToGuild
-);
-app.get("/totalSubscribers", totalSubscribers);
 app.get(
   "/removeExpiredSubscribers",
   secretMiddleware,
